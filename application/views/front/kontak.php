269140b7a65f0a71ad208e771f127a148d829c83
adirahman <kodingku@github.com>
2018-06-02 11:28:18 +0700

    update maps indotrans

--- application/views/front/kontak.php
+++ application/views/front/kontak.php
@@ -1,1 +1,3 @@
-<h1 class="text-center g-mt-50">Under Development</h1>
+<section>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.3907223275137!2d106.84132431535353!3d-6.472091665082969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMjgnMTkuNiJTIDEwNsKwNTAnMzYuNyJF!5e0!3m2!1sen!2sid!4v1527913491582" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</section>
